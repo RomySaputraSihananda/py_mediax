402207a4262e7c4c89215e9423a3be34c35b27e7
romys <romysaputrasihanandaa@gmail.com>
2023-11-09 18:46:18 +0700

    add dokstring

--- src/py_mediax/Mediax.py
+++ src/py_mediax/Mediax.py
@@ -2,6 +2,16 @@ from .py_mediax import py_mediax;
 
 class Mediax:
     def get(url: str) -> object:
+        """
+        This is a method for retrieving data from a Twitter URL
+        
+        Args:
+            url `str` : URL from Twitter that will be scrapped
+
+        Returns:
+            detail `object`: Details of the URL that has been scrapped
+        
+        """
         x = py_mediax();
         
         data = x.get(url);
@@ -11,6 +21,13 @@ class Mediax:
         return data;
 
     def save(folder: str, url: str) -> object:
+        """
+        This is a method to grab an image from a Twitter URL and save it
+        
+        Args:
+            folder `str` : The name of the folder that will be used to store photos
+            url `str` : URL from Twitter that will be scrapped        
+        """
         x = py_mediax();
         
         data = x.save(folder, url);
